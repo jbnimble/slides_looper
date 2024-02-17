01d19e61e16f017feb6d116decac40497195ecec
Jesse <jbnimbledev@gmail.com>
2024-02-17 07:24:56 -0700

    Update app.py
    update --redirect to retrieve the redirect URL first, then load in Chromium
    add --unique flag to add a random 'my_unique=foo' parameter to the redirected URL, an attempt to mitigate caching

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 
 import argparse
 import re
+import urllib
+import random
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import staleness_of
 
-def slide_loader(url: str, is_redirect: bool = False, is_kiosk: bool = False, chrome_debug_port: int = 12345):
+def slide_loader(url: str, is_redirect: bool = False, is_kiosk: bool = False, chrome_debug_port: int = 12345, is_unique: bool = False):
     """ Setup browser specific options, execute  """
     options = webdriver.ChromeOptions()
     options.page_load_strategy = 'normal'
@@ -34,25 +36,27 @@ def slide_loader(url: str, is_redirect: bool = False, is_kiosk: bool = False, ch
 
     # service = webdriver.ChromeService(port=12346) service=service, 
     with webdriver.Chrome(options=options) as driver:
-        slide_repeater(url, is_redirect, driver)
+        slide_repeater(url, is_redirect, is_unique, driver)
 
-def slide_repeater(url: str, is_redirect: bool = False, driver = None):
+def slide_repeater(url: str, is_redirect: bool = False, is_unique: bool = False, driver = None):
     """ Logic for reloading the presentation """
     re_pattern = re.compile(r'^Slide (\d+) of (\d+):')
     wait = WebDriverWait(driver, timeout=30.0)
     data_loaded_url = None
     data_redirect_url = None
     data_latest_aria = None
-    data_latest_groups = None
     print(f'Loading {url}')
     while True:
         # reload loop
         try:
-            driver.get(url)
-            data_loaded_url = driver.current_url
             if is_redirect:
-                wait.until_not(lambda driver: driver.current_url == url)
-                data_redirect_url = driver.current_url
+                data_redirect_url = urllib.request.urlopen(url).geturl()
+                if is_unique:
+                    data_redirect_url = unique_url(data_redirect_url)
+                driver.get(data_redirect_url)
+            else:
+                driver.get(url)
+            data_loaded_url = driver.current_url
 
             while True:
                 # slide loop
@@ -74,9 +78,12 @@ def slide_repeater(url: str, is_redirect: bool = False, driver = None):
             execute_script(driver, 'window.sessionStorage.clear()')
         except Exception as e:
             if str(e).find('target window already closed') > 0:
-                print(f'No window found, exiting')
+                print(f'No window found, exiting loaded={data_loaded_url} redirect={data_redirect_url} aria={data_latest_aria}')
                 break
-            print(f'Exception loaded={data_loaded_url} redirect={data_redirect_url} aria={data_latest_aria} groups={data_latest_groups} error={e}')
+            if str(e).find('stale element not found') > 0:
+                print(f'Stale element not found, reloading loaded={data_loaded_url} redirect={data_redirect_url} aria={data_latest_aria}')
+                continue
+            print(f'Exception loaded={data_loaded_url} redirect={data_redirect_url} aria={data_latest_aria} error={e}')
 
 def execute_script(driver, script: str):
     try:
@@ -84,15 +91,24 @@ def execute_script(driver, script: str):
     except Exception as e:
         print(f'Script "{script}" failed')
 
+def unique_url(url: str):
+    params = {'my_unique': str(random.random())}
+    url_parts = list(urllib.parse.urlparse(url))
+    query = dict(urllib.parse.parse_qsl(url_parts[4]))
+    query.update(params)
+    url_parts[4] = urllib.parse.urlencode(query)
+    return urllib.parse.urlunparse(url_parts)
+
 def main():
     parser = argparse.ArgumentParser(description='Reload and cycle a presentation')
     parser.add_argument('--url', required=True)
     parser.add_argument('--redirect', action='store_true')
     parser.add_argument('--kiosk', action='store_true')
+    parser.add_argument('--unique', action='store_true')
     parser.add_argument('--chrome-debug-port', default=12345)
     args = parser.parse_args()
 
-    slide_loader(url=args.url, is_redirect=args.redirect, is_kiosk=args.kiosk, chrome_debug_port=args.chrome_debug_port)
+    slide_loader(url=args.url, is_redirect=args.redirect, is_kiosk=args.kiosk, chrome_debug_port=args.chrome_debug_port, is_unique=args.unique)
 
 if __name__ == "__main__":
     main()
