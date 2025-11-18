af60b83a1e19335b3a04b6993cecca1f376ff76e
Jesse <jbnimbledev@gmail.com>
2025-11-18 14:32:30 -0700

    Update app.py with driverpath option, rotating log file, and enhanced exception handling

--- app.py
+++ app.py
@@ -8,8 +8,19 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import staleness_of
+from selenium.common.exceptions import InvalidSessionIdException, NoSuchWindowException, StaleElementReferenceException, TimeoutException
+import logging
+from logging.handlers import RotatingFileHandler
 
-def slide_loader(url: str, is_redirect: bool = False, is_kiosk: bool = False, chrome_debug_port: int = 12345, is_unique: bool = False):
+# 5MB rotating log file
+log_format = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
+log_handler = RotatingFileHandler('slides_looper.log', mode='a', maxBytes=5*1024*1024, backupCount=2, encoding='UTF-8', delay=0)
+log_handler.setFormatter(log_format)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(log_handler)
+
+def slide_loader(url: str, is_redirect: bool = False, is_kiosk: bool = False, chrome_debug_port: int = 12345, is_unique: bool = False, driver_path: str = None):
     """ Setup browser specific options, execute  """
     options = webdriver.ChromeOptions()
     options.page_load_strategy = 'normal'
@@ -34,8 +45,13 @@ def slide_loader(url: str, is_redirect: bool = False, is_kiosk: bool = False, ch
     # Removes the "Chrome is being controlled by automated test software" banner
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
 
-    # service = webdriver.ChromeService(port=12346) service=service, 
-    with webdriver.Chrome(options=options) as driver:
+    if driver_path:
+        service = webdriver.ChromeService(executable_path=driver_path)
+    else:
+        service = webdriver.ChromeService()
+    logger.info(f'Create WebDriver with options')
+    with webdriver.Chrome(options=options, service=service) as driver:
+        logger.info(f'Begin slide_repeater')
         slide_repeater(url, is_redirect, is_unique, driver)
 
 def slide_repeater(url: str, is_redirect: bool = False, is_unique: bool = False, driver = None):
@@ -45,7 +61,7 @@ def slide_repeater(url: str, is_redirect: bool = False, is_unique: bool = False,
     data_loaded_url = None
     data_redirect_url = None
     data_latest_aria = None
-    print(f'Loading {url}')
+    logger.info(f'Loading {url}')
     while True:
         # reload loop
         try:
@@ -76,20 +92,19 @@ def slide_repeater(url: str, is_redirect: bool = False, is_unique: bool = False,
             driver.delete_all_cookies()
             execute_script(driver, 'window.localStorage.clear()')
             execute_script(driver, 'window.sessionStorage.clear()')
+        except (InvalidSessionIdException, NoSuchWindowException) as e:
+            logger.warning(f'Exiting due to {type(e)}')
+            break
+        except (StaleElementReferenceException, TimeoutException) as e:
+            logger.warning(f'Reloading due to {type(e)}')
         except Exception as e:
-            if str(e).find('target window already closed') > 0:
-                print(f'No window found, exiting loaded={data_loaded_url} redirect={data_redirect_url} aria={data_latest_aria}')
-                break
-            if str(e).find('stale element not found') > 0:
-                print(f'Stale element not found, reloading loaded={data_loaded_url} redirect={data_redirect_url} aria={data_latest_aria}')
-                continue
-            print(f'Exception loaded={data_loaded_url} redirect={data_redirect_url} aria={data_latest_aria} error={e}')
+            logger.error(f'Reloading due to unknown exception={type(e)}\n\turl={data_loaded_url}\n\tredirect_url={data_redirect_url}\n\taria_data={data_latest_aria}\n\terror={e}')
 
 def execute_script(driver, script: str):
     try:
         driver.execute_script(script)
     except Exception as e:
-        print(f'Script "{script}" failed')
+        logger.error(f'Script "{script}" failed')
 
 def unique_url(url: str):
     params = {'my_unique': str(random.random())}
@@ -106,9 +121,10 @@ def main():
     parser.add_argument('--kiosk', action='store_true')
     parser.add_argument('--unique', action='store_true')
     parser.add_argument('--chrome-debug-port', default=12345)
+    parser.add_argument('--driverpath')
     args = parser.parse_args()
 
-    slide_loader(url=args.url, is_redirect=args.redirect, is_kiosk=args.kiosk, chrome_debug_port=args.chrome_debug_port, is_unique=args.unique)
+    slide_loader(url=args.url, is_redirect=args.redirect, is_kiosk=args.kiosk, chrome_debug_port=args.chrome_debug_port, is_unique=args.unique, driver_path=args.driverpath)
 
 if __name__ == "__main__":
     main()
