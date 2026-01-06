4241c85ae59ad79c34636430ea3ac2c6c8469be5
Jesse <jbnimbledev@gmail.com>
2026-01-06 14:37:46 -0700

    Update app.py
    Add additional flags:
      --maximize
      --loop-new-window
      --last-slide-wait-sec
    Refactored to use AppOptions class

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import argparse
 import re
 import urllib
 import random
+import time
+import logging
+from logging.handlers import RotatingFileHandler
 from selenium import webdriver
+from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import staleness_of
 from selenium.common.exceptions import InvalidSessionIdException, NoSuchWindowException, StaleElementReferenceException, TimeoutException
-import logging
-from logging.handlers import RotatingFileHandler
 
 # 5MB rotating log file
 log_format = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
@@ -20,59 +22,82 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(log_handler)
 
-def slide_loader(url: str, is_redirect: bool = False, is_kiosk: bool = False, chrome_debug_port: int = 12345, is_unique: bool = False, driver_path: str = None):
-    """ Setup browser specific options, execute  """
-    options = webdriver.ChromeOptions()
-    options.page_load_strategy = 'normal'
-    options.accept_insecure_certs = True
-    options.add_argument('no-default-browser-check')
-    options.add_argument('no-first-run')
-    options.add_argument('ash-no-nudges')
-    options.add_argument('disable-search-engine-choice-screen')
-    options.add_argument('aggressive-cache-discard')
-    options.add_argument('deny-permission-prompts')
-    options.add_argument('disable-notifications')
-    options.add_argument('disk-cache-size=0')
-    options.add_argument('disable-background-networking')
-    options.add_argument('disable-component-update')
-    options.add_argument('disable-domain-reliability')
-    options.add_argument('disable-sync')
-    options.add_argument('no-pings')
-    # stops this error => "DevToolsActivePort file doesn't exist"
-    options.add_argument(f'remote-debugging-port={chrome_debug_port}')
-    if is_kiosk:
-        options.add_argument('kiosk')
-    # Removes the "Chrome is being controlled by automated test software" banner
-    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+class AppOptions:
+    def __init__(self, url, is_redirect, is_kiosk, is_f11, is_unique, is_maximize, is_loop_new_window, last_slide_wait_sec, chrome_debug_port, driver_path):
+        self.url = url
+        self.is_redirect = is_redirect
+        self.is_kiosk = is_kiosk
+        self.is_f11 = is_f11
+        self.is_unique = is_unique
+        self.is_maximize = is_maximize
+        self.is_loop_new_window = is_loop_new_window
+        self.last_slide_wait_sec = last_slide_wait_sec
+        self.chrome_debug_port = chrome_debug_port
+        self.driver_path = driver_path
 
-    if driver_path:
-        service = webdriver.ChromeService(executable_path=driver_path)
+def slide_loader(appOptions: AppOptions):
+    """ Setup browser specific options, execute  """
+    webOptions = webdriver.ChromeOptions()
+    webOptions.page_load_strategy = 'normal'
+    webOptions.accept_insecure_certs = True
+    webOptions.add_argument('no-default-browser-check')
+    webOptions.add_argument('no-first-run')
+    webOptions.add_argument('ash-no-nudges')
+    webOptions.add_argument('disable-search-engine-choice-screen')
+    webOptions.add_argument('aggressive-cache-discard')
+    webOptions.add_argument('deny-permission-prompts')
+    webOptions.add_argument('disable-notifications')
+    webOptions.add_argument('disk-cache-size=0')
+    webOptions.add_argument('disable-background-networking')
+    webOptions.add_argument('disable-component-update')
+    webOptions.add_argument('disable-domain-reliability')
+    webOptions.add_argument('disable-sync')
+    webOptions.add_argument('no-pings')
+    # stops this error => "DevToolsActivePort file doesn't exist"
+    webOptions.add_argument(f'remote-debugging-port={appOptions.chrome_debug_port}')
+    if appOptions.is_kiosk:
+        webOptions.add_argument('kiosk')
+    # Removes the "Chrome is being controlled by automated test software" banner
+    webOptions.add_experimental_option('excludeSwitches', ['enable-automation'])
+
+    if appOptions.driver_path:
+        service = webdriver.ChromeService(executable_path=appOptions.driver_path)
     else:
         service = webdriver.ChromeService()
     logger.info(f'Create WebDriver with options')
-    with webdriver.Chrome(options=options, service=service) as driver:
+    repeater_result = False
+    with webdriver.Chrome(options=webOptions, service=service) as driver:
         logger.info(f'Begin slide_repeater')
-        slide_repeater(url, is_redirect, is_unique, driver)
+        repeater_result = slide_repeater(appOptions, driver)
+    return repeater_result
 
-def slide_repeater(url: str, is_redirect: bool = False, is_unique: bool = False, driver = None):
-    """ Logic for reloading the presentation """
+def slide_repeater(appOptions: AppOptions, driver) -> bool:
+    """ Logic for reloading the presentation, return false to not loop again """
     re_pattern = re.compile(r'^Slide (\d+) of (\d+):')
     wait = WebDriverWait(driver, timeout=30.0)
     data_loaded_url = None
     data_redirect_url = None
     data_latest_aria = None
-    logger.info(f'Loading {url}')
+    logger.info(f'Loading {appOptions.url}')
+
+    if appOptions.is_maximize:
+        driver.maximize_window() # Maximize window
+
     while True:
         # reload loop
         try:
-            if is_redirect:
-                data_redirect_url = urllib.request.urlopen(url).geturl()
-                if is_unique:
+            data_loaded_url = driver.current_url
+
+            if appOptions.is_redirect:
+                data_redirect_url = urllib.request.urlopen(appOptions.url).geturl()
+                if appOptions.is_unique:
                     data_redirect_url = unique_url(data_redirect_url)
                 driver.get(data_redirect_url)
             else:
-                driver.get(url)
-            data_loaded_url = driver.current_url
+                driver.get(appOptions.url)
+
+            if appOptions.is_f11:
+                ActionChains(driver).send_keys(Keys.F11).perform() # make fullscreen by pressing F11
 
             while True:
                 # slide loop
@@ -81,8 +106,15 @@ def slide_repeater(url: str, is_redirect: bool = False, is_unique: bool = False,
                 re_matches = re_pattern.match(data_latest_aria)
 
                 if len(re_matches.groups()) > 1 and re_matches.group(1) == re_matches.group(2):
+                    time_to_wait = float(appOptions.last_slide_wait_sec)
+                    if time_to_wait >= 1:
+                        time.sleep(time_to_wait) # pause before breaking to reload loop
                     latest_groups = f'{re_matches.group(1)} of {re_matches.group(2)}'
-                    break
+                    if appOptions.is_loop_new_window:
+                        logger.info(f'Exiting due to is_loop_new_window={appOptions.is_loop_new_window}')
+                        return True
+                    else:
+                        break
                 elif len(re_matches.groups()) > 1:
                     latest_groups = f'{re_matches.group(1)} of {re_matches.group(2)}'
                 else:
@@ -99,6 +131,7 @@ def slide_repeater(url: str, is_redirect: bool = False, is_unique: bool = False,
             logger.warning(f'Reloading due to {type(e)}')
         except Exception as e:
             logger.error(f'Reloading due to unknown exception={type(e)}\n\turl={data_loaded_url}\n\tredirect_url={data_redirect_url}\n\taria_data={data_latest_aria}\n\terror={e}')
+    return False
 
 def execute_script(driver, script: str):
     try:
@@ -116,15 +149,35 @@ def unique_url(url: str):
 
 def main():
     parser = argparse.ArgumentParser(description='Reload and cycle a presentation')
-    parser.add_argument('--url', required=True)
-    parser.add_argument('--redirect', action='store_true')
-    parser.add_argument('--kiosk', action='store_true')
-    parser.add_argument('--unique', action='store_true')
-    parser.add_argument('--chrome-debug-port', default=12345)
-    parser.add_argument('--driverpath')
+    parser.add_argument('--url', required=True, help='presentation URL')
+    parser.add_argument('--redirect', action='store_true', help='Query for potential redirect URL')
+    parser.add_argument('--kiosk', action='store_true', help='Enable kiosk mode')
+    parser.add_argument('--f11', action='store_true', help='Press F11 key when after loading URL')
+    parser.add_argument('--unique', action='store_true', help='Add unique parameter with random value to URL')
+    parser.add_argument('--maximize', action='store_true', help='Start browser window maximized')
+    parser.add_argument('--loop-new-window', action='store_true', help='Start new browser for each loop')
+    parser.add_argument('--last-slide-wait-sec', default="0", help='Wait number of seconds on last slide')
+    parser.add_argument('--chrome-debug-port', default=12345, help='Chrome debug port')
+    parser.add_argument('--driverpath', default='/usr/bin/chromedriver', help='Filesystem path to Chrome driver executable')
     args = parser.parse_args()
+    appOptions = AppOptions(url=args.url, 
+        is_redirect = args.redirect, 
+        is_kiosk = args.kiosk, 
+        is_f11 = args.f11, 
+        is_unique = args.unique, 
+        is_maximize = args.maximize, 
+        is_loop_new_window = args.loop_new_window, 
+        last_slide_wait_sec = args.last_slide_wait_sec, 
+        chrome_debug_port=args.chrome_debug_port, 
+        driver_path=args.driverpath)
 
-    slide_loader(url=args.url, is_redirect=args.redirect, is_kiosk=args.kiosk, chrome_debug_port=args.chrome_debug_port, is_unique=args.unique, driver_path=args.driverpath)
+    if appOptions.is_loop_new_window:
+        loop_again = True
+        while loop_again:
+            loop_again = slide_loader(appOptions)
+        logger.info(f'Exiting due to loop_again={loop_again}')
+    else:
+        slide_loader(appOptions)
 
 if __name__ == "__main__":
     main()
